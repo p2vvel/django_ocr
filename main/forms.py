358fbcaa1048f2cbe921adc621fd88db51e7102f
p2vvel <p2vvel@gmail.com>
2021-09-22 20:44:41 +0200

    Image rotation fix todo

--- main/forms.py
+++ main/forms.py
@@ -6,6 +6,9 @@ class ImageForm(forms.ModelForm):
     class Meta:
         model = ImageModel
         fields = ["file"]
+        widgets = {
+            'file': forms.FileInput(attrs={"class": "form-control form-control-lg"})
+        }
 
 
 
@@ -17,7 +20,8 @@ rotate_options = (
 )
 
 
+
 class TransformationFrom(forms.Form):
-    rotation = forms.ChoiceField(choices=rotate_options, label="Rotation", initial="0", widget=forms.RadioSelect)
-    mirror_x = forms.BooleanField(label="Mirror X", initial=False, required=False)
-    mirror_y = forms.BooleanField(label="Mirror Y", initial=False, required=False)
+    rotation = forms.ChoiceField(choices=rotate_options, label="Rotation", initial="0", widget=forms.RadioSelect(attrs={"class": "form-check"}))
+    mirror_x = forms.BooleanField(label="Mirror X", initial=False, required=False, widget=forms.CheckboxInput(attrs={"class": "form-check-input"}))
+    mirror_y = forms.BooleanField(label="Mirror Y", initial=False, required=False, widget=forms.CheckboxInput(attrs={"class": "form-check-input"}))
